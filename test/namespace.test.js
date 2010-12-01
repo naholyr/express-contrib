a66c396f7250a28a1a0c55cd8f60ffe623198c6c
Tj Holowaychuk <tj@vision-media.ca>
2010-11-30 21:46:37 -0800

    Added nested namespacing support

--- test/namespace.test.js
+++ test/namespace.test.js
@@ -14,7 +14,6 @@ module.exports = {
       res.send('GET one');
     });
 
-    assert.equal(app.namespace('/user'), app);
     assert.equal(app.namespace('/user', function(){}), app);
 
     app.namespace('/user', function(){
@@ -26,46 +25,6 @@ module.exports = {
         res.send('DELETE user ' + req.params.id);
       });
     });
-    
-    app.get('/two', function(req, res){
-      res.send('GET two');
-    });
-
-    assert.response(app,
-      { url: '/user/12' },
-      { body: 'GET user 12' });
-  
-    assert.response(app,
-      { url: '/user/12', method: 'DELETE' },
-      { body: 'DELETE user 12' });
-    
-    assert.response(app,
-      { url: '/one' },
-      { body: 'GET one' });
-    
-    assert.response(app,
-      { url: '/two' },
-      { body: 'GET two' });
-  },
-  
-  'test app.namespace(str)': function(assert){
-    var app = express.createServer();
-
-    app.get('/one', function(req, res){
-      res.send('GET one');
-    });
-
-    app.namespace('/user');
-
-    app.get('/:id', function(req, res){
-      res.send('GET user ' + req.params.id);
-    });
-    
-    app.del('/:id', function(req, res){
-      res.send('DELETE user ' + req.params.id);
-    });
-
-    app.namespace('/');
     
     app.get('/two', function(req, res){
       res.send('GET two');
