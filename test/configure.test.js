5a9db0a95fc220e673b412de264f3d48b5ac8652
Tj Holowaychuk <tj@vision-media.ca>
2010-12-06 13:20:30 -0800

    Added async configure tests

---
+++ test/configure.test.js
@@ -0,0 +1,71 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , contrib = require('express-contrib')
+  , assert = require('assert');
+
+// Faux redis
+
+var redis = {
+  store: {},
+  hmget: function(key, fn){
+    var self = this;
+    process.nextTick(function(){
+      fn(null, self.store[key]);
+    });
+  },
+  
+  hmset: function(key, obj, fn){
+    var self = this;
+    process.nextTick(function(){
+      self.store[key] = obj;
+      fn();
+    });
+  }
+}
+
+module.exports = {
+  setup: function(done){
+    redis.hmset('settings', {
+        title: 'Async example'
+      , foo: 'bar'
+      , bar: 'baz'
+    }, done);
+  },
+
+  'test async configure()': function(){
+    var app = express.createServer();
+
+    app.configure(function(done){
+      redis.hmget('settings', function(err, obj){
+        for (var key in obj) {
+          app.set(key, obj[key]);
+        }
+        done();
+      });
+    });
+
+    app.configure(function(done){
+      app.set('tobi', 'is cool');
+      done();
+    });
+
+    app.configure(function(){
+      app.enable('sync');
+    });
+
+    app.on('listening', function(){
+      assert.strictEqual(true, app.set('sync'), 'sync configure() never called');
+      assert.equal('is cool', app.set('tobi'));
+      assert.equal('Async example', app.set('title'));
+      assert.equal('bar', app.set('foo'));
+      assert.equal('baz', app.set('bar'));
+    });
+
+    app.listen(9999);
+    assert.ok(!app.fd, 'listen() was not deferred');
+  }
+};
